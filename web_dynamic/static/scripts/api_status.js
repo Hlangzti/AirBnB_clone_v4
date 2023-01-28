01598632fa3aeee96510b693673fcd3596831712
cappochinho <edwardprempeh18@gmail.com>
2023-01-28 13:56:05 +0000

    Added from task 1 to #3

---
+++ web_dynamic/static/scripts/api_status.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    $("header").append("<div id='api_status'></div>");
+    $("#api_status").css({
+        "float": "right",
+        "border-radius": "50%",
+        "display": "flex",
+        "align-items": "center",
+        "margin-right": "30px",
+        "background-color": "#cccccc",
+        "width": "40px",
+        "height": "40px"
+    });
+});
